73eee2c77d07154845d9908b21b75c58c02a4cef
Balakosh <akos.balassa@live.com>
2018-11-10 16:52:29 +0100

    inital commit

---
+++ empty.c
@@ -0,0 +1,66 @@
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+
+/* TI-RTOS Header files */
+// #include <ti/drivers/EMAC.h>
+#include <ti/drivers/GPIO.h>
+// #include <ti/drivers/I2C.h>
+// #include <ti/drivers/SDSPI.h>
+// #include <ti/drivers/SPI.h>
+// #include <ti/drivers/UART.h>
+// #include <ti/drivers/USBMSCHFatFs.h>
+// #include <ti/drivers/Watchdog.h>
+// #include <ti/drivers/WiFi.h>
+
+#include "Board.h"
+
+#define TASKSTACKSIZE   512
+
+Task_Struct task0Struct;
+Char task0Stack[TASKSTACKSIZE];
+
+Void heartBeatFxn(UArg arg0, UArg arg1)
+{
+    while (1) {
+        Task_sleep((unsigned int)arg0);
+        GPIO_toggle(Board_LED0);
+    }
+}
+
+int main(void)
+{
+    Task_Params taskParams;
+    /* Call board init functions */
+    Board_initGeneral();
+    // Board_initEMAC();
+    Board_initGPIO();
+    // Board_initI2C();
+    // Board_initSDSPI();
+    // Board_initSPI();
+    // Board_initUART();
+    // Board_initUSB(Board_USBDEVICE);
+    // Board_initUSBMSCHFatFs();
+    // Board_initWatchdog();
+    // Board_initWiFi();
+
+    /* Construct heartBeat Task  thread */
+    Task_Params_init(&taskParams);
+    taskParams.arg0 = 1000;
+    taskParams.stackSize = TASKSTACKSIZE;
+    taskParams.stack = &task0Stack;
+    Task_construct(&task0Struct, (Task_FuncPtr)heartBeatFxn, &taskParams, NULL);
+
+    GPIO_write(Board_LED0, Board_LED_ON);
+
+    System_printf("Starting the example\nSystem provider is set to SysMin. "
+                  "Halt the target to view any SysMin contents in ROV.\n");
+    /* SysMin will only print to the console when you call flush or exit */
+    System_flush();
+
+    BIOS_start();
+
+    return (0);
+}
